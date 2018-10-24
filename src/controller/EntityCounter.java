e7c5f0dd6ca62416bf8a5009bc347e86d51eadd8
seyedalireza <shosseini@ce.sharif.edu>
2018-10-24 23:23:06 +0330

    debug

--- src/controller/EntityCounter.java
+++ src/controller/EntityCounter.java
@@ -2,90 +2,60 @@ package controller;
 
 public class EntityCounter {
 
-    private int homeNumber = 0;
-    private int floorNumber = 0;
-    private int unitNumber = 0;
     private int blockNumber = 0;
-    private int armyNumber = 0;
-    private int defenderNumber = 0;
-    private int marketNumber = 0;
-    private int parkNumber = 0;
+    private int entityNumber = 0;
     private int personNumber;
 
     public EntityCounter() {
     }
 
     public int getHomeNumber() {
-        homeNumber++;
-        return homeNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setHomeNumber(int homeNumber) {
-        this.homeNumber = homeNumber;
-    }
 
     public int getFloorNumber() {
-        floorNumber++;
-        return floorNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setFloorNumber(int floorNumber) {
-        this.floorNumber = floorNumber;
-    }
 
     public int getUnitNumber() {
-        unitNumber++;
-        return unitNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setUnitNumber(int unitNumber) {
-        this.unitNumber = unitNumber;
-    }
 
     public int getBlockNumber() {
         blockNumber++;
         return blockNumber;
     }
 
-    public void setBlockNumber(int blockNumber) {
-        this.blockNumber = blockNumber;
-    }
 
     public int getArmyNumber() {
-        armyNumber++;
-        return armyNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setArmyNumber(int armyNumber) {
-        this.armyNumber = armyNumber;
-    }
 
     public int getDefenderNumber() {
-        defenderNumber++;
-        return defenderNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setDefenderNumber(int defenderNumber) {
-        this.defenderNumber = defenderNumber;
-    }
 
     public int getMarketNumber() {
-        marketNumber++;
-        return marketNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setMarketNumber(int marketNumber) {
-        this.marketNumber = marketNumber;
-    }
 
     public int getParkNumber() {
-        parkNumber++;
-        return parkNumber;
+        entityNumber++;
+        return entityNumber;
     }
 
-    public void setParkNumber(int parkNumber) {
-        this.parkNumber = parkNumber;
-    }
 
     public int getPersonNumber() {
         personNumber++;
